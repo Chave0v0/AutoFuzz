962a57f44cce17190b78f36b5864e2f23eb6d3f0
Chave0v0 <chennehc1024@gmail.com>
2025-06-01 21:17:34 +0800

    fix: 修复 AuthHeader 编辑问题

--- src/main/java/com/chave/utils/YamlUtil.java
+++ src/main/java/com/chave/utils/YamlUtil.java
@@ -55,14 +55,16 @@ public class YamlUtil {
                         Data.PAYLOAD_LIST.add((String) payloadmap.get("value"));
                     }
                 }
-                //@d1sbb修改，加载 domain
+
+                // @d1sbb修改，加载 domain
                 ArrayList<LinkedHashMap> domain = (ArrayList<LinkedHashMap>) map.get("domain");
                 if (domain != null) {
                     for (LinkedHashMap domainMap : domain) {
                         Data.DOMAIN_LIST.add((String) domainMap.get("value"));
                     }
                 }
-                //@d1sbb修改，header
+
+                // @d1sbb修改，header
                 ArrayList<LinkedHashMap> header = (ArrayList<LinkedHashMap>) map.get("header");
                 if (header != null) {
                     for (LinkedHashMap headerMap : header) {
